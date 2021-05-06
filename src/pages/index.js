ab7d28651dae8ef6868589d41debf01b374d94cf
Scott Janssen <scottmjanssen@gmail.com>
2021-05-06 15:23:15 -0400

    Added some pages and a header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import Header from "../components/header"
+import {Link} from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div style={{color: `red`, fontSize: `50px`}}>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="Hello Gatsby!" />
+  </div>
 }
